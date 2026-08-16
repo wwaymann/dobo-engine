c076fba05820b97db45392a5f7ea2e668482d016
Walter Waymann <59487134+wwaymann@users.noreply.github.com>
2026-08-15 22:17:18 -0400

    feat(c): add production package module

---
+++ app/product_generators/production_package/__init__.py
@@ -0,0 +1,19 @@
+from .bundle import (
+    ArtifactRecord,
+    GenerationPackageManifest,
+    ProductionPackageBuilder,
+)
+from .render_contract import (
+    RenderContract,
+    RenderIntent,
+    RenderView,
+)
+
+__all__ = [
+    "ArtifactRecord",
+    "GenerationPackageManifest",
+    "ProductionPackageBuilder",
+    "RenderContract",
+    "RenderIntent",
+    "RenderView",
+]
